d2cf900e64ded2cbfb0db77dd7577c6e50acdf3b
kollin78 <113921000+kollin78@users.noreply.github.com>
2026-01-09 08:36:32 -0700

    bishop move progress

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,7 +1,6 @@
 package chess;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Represents a single chess piece
@@ -54,11 +53,47 @@ public class ChessPiece {
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
+        List<ChessMove> validMoves = new ArrayList<>();
+        ChessMove newMove;
+        ChessPosition moveToPos;
+
         if(piece.getPieceType() == PieceType.BISHOP) {
+            int startRow = myPosition.getRow();
+            int startCol = myPosition.getColumn();
 
-            for(int row; row < 8; row++) {
+            Set<ChessMove> setMoves = new LinkedHashSet<>(validMoves);
 
+
+            for(int i = 1; i < 8; i++) {
+                if((startRow+i < 8) && (startCol+i < 8)) {
+                    moveToPos = new ChessPosition(startRow+i, startCol+i);
+                    newMove = new ChessMove(myPosition, moveToPos, null);
+                    setMoves.add(newMove);
+                }
             }
+            for(int i = 1; i < 8; i++) {
+                if((startRow-i >= 0) && (startCol+i < 8)) {
+                    moveToPos = new ChessPosition(startRow-i, startCol+i);
+                    newMove = new ChessMove(myPosition, moveToPos, null);
+                    setMoves.add(newMove);
+                }
+            }
+            for(int i = 1; i < 8; i++) {
+                if((startRow-i >= 0) && (startCol-i >= 0)) {
+                    moveToPos = new ChessPosition(startRow-i, startCol-i);
+                    newMove = new ChessMove(myPosition, moveToPos, null);
+                    setMoves.add(newMove);
+                }
+            }
+            for(int i = 1; i < 8; i++) {
+                if((startRow+i < 8) && (startCol-i >= 0)) {
+                    moveToPos = new ChessPosition(startRow+i, startCol-i);
+                    newMove = new ChessMove(myPosition, moveToPos, null);
+                    setMoves.add(newMove);
+                }
+            }
+
+            return new ArrayList<>(setMoves);
             //return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
         }
 
