8f940268b29e2e6a552ac7b7dedc4b6a92e40022
Kollin <kollroge78@gmail.com>
2026-02-12 22:58:19 -0700

    implemented ClearService

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,20 +1,33 @@
 package server;
 
-import dataaccess.UserDAO;
-import dataaccess.UserDAOMemory;
+import dataaccess.*;
 import io.javalin.*;
+import service.ClearService;
 
 public class Server {
 
     private final Javalin javalin;
 
     private final UserDAO userDAO = new UserDAOMemory();
+    private final AuthDAO authDAO = new AuthDAOMemory();
+    private final GameDAO gameDAO = new GameDAOMemory();
 
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+
+        javalin.delete("/db", (req) -> {
+            try {
+                new ClearService(userDAO, authDAO, gameDAO).clearAll();
+                req.status(200);
+                req.result();
+            } catch (DataAccessException e) {
+                req.status(500);
+                req.result("{\"message\": \"Error: " + e.getMessage() + "\" }");
+            }
+        });
 
     }
 
