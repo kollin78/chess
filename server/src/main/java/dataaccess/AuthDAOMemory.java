fbfc00650f80b130d6d212e0687d3f4c07286ba1
Kollin <kollroge78@gmail.com>
2026-02-12 22:12:23 -0700

    DAO progress

--- server/src/main/java/dataaccess/AuthDAOMemory.java
+++ server/src/main/java/dataaccess/AuthDAOMemory.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
-public class AuthDAOMemory {
+import model.AuthData;
+
+import java.util.HashMap;
+
+public class AuthDAOMemory implements AuthDAO {
+    private final HashMap<String, AuthData> authDatas = new HashMap<>();
+
+    @Override
+    public void createAuth(AuthData authData) throws DataAccessException {
+        authDatas.put(authData.authToken(), authData);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return authDatas.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authDatas.remove(authToken);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        authDatas.clear();
+    }
 }
--- server/src/main/java/dataaccess/GameDAOMemory.java
+++ server/src/main/java/dataaccess/GameDAOMemory.java
@@ -1,4 +1,37 @@
 package dataaccess;
 
-public class GameDAOMemory {
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+public class GameDAOMemory implements GameDAO{
+    private final HashMap<Integer, GameData> games = new HashMap<>();
+    private int gameID = 1234;
+
+    @Override
+    public int createGame(String gameName) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
 }
