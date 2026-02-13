7da44eacb6d702103a28d93557e394ab316167a3
Kollin <kollroge78@gmail.com>
2026-02-12 22:45:11 -0700

    DAO progress

--- server/src/main/java/dataaccess/GameDAOMemory.java
+++ server/src/main/java/dataaccess/GameDAOMemory.java
@@ -1,7 +1,9 @@
 package dataaccess;
 
 import model.GameData;
+import chess.ChessGame;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -12,26 +14,34 @@ public class GameDAOMemory implements GameDAO{
 
     @Override
     public int createGame(String gameName) throws DataAccessException {
-        return 0;
+        GameData data = new GameData(gameID, "", "", gameName, new ChessGame());
+        games.put(gameID, data);
+        gameID++;
+        return gameID - 1; // returns gameID for this game, -1 bc you updated gameID before leaving function
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
-        return null;
+        return games.get(gameID);
     }
 
     @Override
     public Collection<GameData> listGames() throws DataAccessException {
-        return List.of();
+        return new ArrayList<>(games.values());
     }
 
     @Override
     public void updateGame(GameData game) throws DataAccessException {
+        if(!games.containsKey(game.gameID())) {
+            throw new DataAccessException("Error: Game Not Found");
+        }
 
+        games.put(game.gameID(), game);
     }
 
     @Override
     public void clear() throws DataAccessException {
-
+        games.clear();
+        gameID = 1234;
     }
 }
