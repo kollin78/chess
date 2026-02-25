5166e3ffe3131dba4d68d8e9b3737ef2ce0b3c9f
kollin78 <kollroge78@gmail.com>
2026-02-24 20:16:12 -0700

    Adding handlers to Server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,16 @@
 package server;
 
+import com.google.gson.Gson;
 import dataaccess.*;
 import io.javalin.*;
+import io.javalin.http.Context;
+import io.javalin.http.InternalServerErrorResponse;
+import model.AuthResult;
+import model.ErrorResponse;
+import model.RegisterRequest;
+import org.eclipse.jetty.server.Authentication;
 import service.ClearService;
+import service.UserService;
 
 public class Server {
 
@@ -12,25 +20,17 @@ public class Server {
     private final AuthDAO authDAO = new AuthDAOMemory();
     private final GameDAO gameDAO = new GameDAOMemory();
 
+    private final UserService userService = new UserService(userDAO, authDAO);
+    private final ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
 
-        javalin.delete("/db", (req) -> {
-            try {
-                new ClearService(userDAO, authDAO, gameDAO).clearAll();
-                req.status(200);
-                req.result();
-            } catch (DataAccessException e) {
-                req.status(500);
-                req.result("{\"message\": \"Error: " + e.getMessage() + "\" }");
-            }
-        });
-        javalin.post("/user", (req) -> {
-
-        });
+        javalin.delete("/db", this::clearHandler);
+        javalin.post("/user", this::registerHandler);
+        javalin.post("/session", this::loginHandler);
 
     }
 
@@ -41,5 +41,38 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    private void clearHandler(Context context) {
+        try {
+            new ClearService(userDAO, authDAO, gameDAO).clearAll();
+            context.status(200);
+            context.result("{}");
+        } catch (DataAccessException e) {
+            context.status(500);
+            context.json(new ErrorResponse(e.getMessage()));
+        }
+    }
+
+    private void registerHandler(Context context) {
+        try {
+            var req = new Gson().fromJson(context.body(), RegisterRequest.class);
+            AuthResult result = userService.registerUser(req);
+            context.status(200);
+            context.json(result);
+        } catch(DataAccessException e) {
+            if(e.getMessage().contains("bad request")) {
+                context.status(400);
+            } else if (e.getMessage().contains("username already taken")) {
+                context.status(403);
+            } else {
+                context.status(500);
+            }
+            context.json(new ErrorResponse(e.getMessage()));
+        }
+    }
+
+    private void loginHandler(Context context) {
+
     }
 }
