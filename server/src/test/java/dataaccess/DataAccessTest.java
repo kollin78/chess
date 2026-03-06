6d450914544d1aa2962a91711f2670f17f7a577d
kollin78 <kollroge78@gmail.com>
2026-03-05 18:00:38 -0700

    gameDAO tests

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -1,6 +1,8 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.AuthData;
+import model.GameData;
 import model.UserData;
 import org.junit.jupiter.api.*;
 
@@ -81,5 +83,119 @@ public class DataAccessTest {
         dataAccess.createAuth(testAuth);
 
         assertThrows(DataAccessException.class, () -> dataAccess.createAuth(new AuthData("realAuthToken", "testUser")));
+    }
+
+    @Test
+    @DisplayName("get auth success")
+    public void getAuthSuccess() throws DataAccessException {
+        UserData createdUser = new UserData("testUser", "testPass", "test@byu.net");
+        dataAccess.createUser(createdUser);
+        AuthData testAuth = new AuthData("realAuthToken", "testUser");
+        dataAccess.createAuth(testAuth);
+
+        assertEquals(testAuth, dataAccess.getAuth("realAuthToken"));
+    }
+
+    @Test
+    @DisplayName("get auth epic fail")
+    public void getAuthFail() throws DataAccessException {
+        UserData createdUser = new UserData("testUser", "testPass", "test@byu.net");
+        dataAccess.createUser(createdUser);
+        AuthData testAuth = new AuthData("realAuthToken", "testUser");
+        dataAccess.createAuth(testAuth);
+
+        assertThrows(DataAccessException.class, () -> dataAccess.getAuth("randomToken"));
+    }
+
+    @Test
+    @DisplayName("delete auth success")
+    public void deleteAuthSuccess() throws DataAccessException {
+        UserData createdUser = new UserData("testUser", "testPass", "test@byu.net");
+        dataAccess.createUser(createdUser);
+        AuthData testAuth = new AuthData("realAuthToken", "testUser");
+        dataAccess.createAuth(testAuth);
+        dataAccess.deleteAuth("realAuthToken");
+
+        assertNull(dataAccess.getAuth("realAuthToken"));
+    }
+
+    @Test
+    @DisplayName("delete auth epic fail")
+    public void deleteAuthFail() throws DataAccessException {
+        UserData createdUser = new UserData("testUser", "testPass", "test@byu.net");
+        dataAccess.createUser(createdUser);
+        AuthData testAuth = new AuthData("realAuthToken", "testUser");
+        dataAccess.createAuth(testAuth);
+
+        assertThrows(DataAccessException.class, () -> dataAccess.deleteAuth("randomToken"));
+    }
+
+    //createGame, getGame, listGames, updateGame
+
+    @Test
+    @DisplayName("create game success")
+    public void createGameSuccess() throws DataAccessException {
+        int gameID = dataAccess.createGame("realGame");
+
+        assertNotNull(dataAccess.getGame(gameID));
+    }
+
+    @Test
+    @DisplayName("create game fail")
+    public void createGameFail() throws DataAccessException {
+
+        assertThrows(DataAccessException.class, () -> dataAccess.createGame(null));
+    }
+
+    @Test
+    @DisplayName("get game success")
+    public void getGameSuccess() throws DataAccessException {
+        int gameID = dataAccess.createGame("realGame");
+
+        assertEquals("realGame", dataAccess.getGame(gameID).gameName());
+    }
+
+    @Test
+    @DisplayName("get game fail")
+    public void getGameFail() throws DataAccessException {
+
+        assertThrows(DataAccessException.class, () -> dataAccess.getGame(4321));
+    }
+
+    @Test
+    @DisplayName("list game success")
+    public void listGameSuccess() throws DataAccessException {
+        GameData gameData = new GameData(1234, "whiteUsername", "blackUsername", "realGame", new ChessGame());
+        dataAccess.createGame("realGame");
+
+        assertNotNull(dataAccess.listGames());
+    }
+
+    @Test
+    @DisplayName("list game fail")
+    public void listGameFail() throws DataAccessException {
+
+        assertTrue(dataAccess.listGames().isEmpty());
+    }
+
+    @Test
+    @DisplayName("update game success")
+    public void updateGameSuccess() throws DataAccessException {
+        int gameID = dataAccess.createGame("createdGame");
+        GameData realGame = new GameData(gameID, "whiteUsername", "blackUsername", "realGame", new ChessGame());
+        dataAccess.updateGame(realGame);
+        GameData retrievedGame = dataAccess.getGame(gameID);
+
+        assertNotNull(retrievedGame);
+        assertEquals("createdGame", retrievedGame.gameName());
+    }
+
+    @Test
+    @DisplayName("update game fail")
+    public void updateGameFail() throws DataAccessException {
+        int gameID = dataAccess.createGame("createdGame");
+        GameData sadGame = new GameData(gameID, "whiteUsername", "blackUsername", "superSadGame", null);
+
+        assertThrows(DataAccessException.class, () -> dataAccess.updateGame(sadGame));
     }
 }
