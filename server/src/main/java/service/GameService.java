794d74e8f8bd3c44a6367aea3331f854c1179696
kollin78 <kollroge78@gmail.com>
2026-03-23 15:44:00 -0600

    fixe GameService for spectateGame

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -40,8 +40,8 @@ public class GameService {
         if(gameData == null) {
             throw new DataAccessException("Error: bad request");
         }
-        if((playerColor == null) || (playerColor.isEmpty())) {
-            throw new DataAccessException("Error: bad request");
+        if(playerColor == null) {
+            return;
         }
 
         String whiteUser = gameData.whiteUsername();
